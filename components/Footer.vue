61939b851710c6b874c288eba8fe2e72d5f81bf5
nitesh-dev <niteshdev547@gmail.com>
2023-08-28 14:38:13 +0530

    footer competed

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,9 +2,312 @@
 </script>
 <template>
     <footer>
-        <div class="page"></div>
+        <div class="page">
+
+            <!-- form -->
+            <form class="desktop">
+                <div class="inner">
+                    <h3>Join Our Newsletter</h3>
+                    <div class="input">
+                        <input type="text" placeholder="Enter your email..">
+                        <button type="submit">Subscribe</button>
+                    </div>
+                </div>
+
+                <!-- outline -->
+                <svg class="circle" xmlns="http://www.w3.org/2000/svg" width="483" height="243" viewBox="0 0 483 243" fill="none">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M241.505 75.9838C333.224 75.9838 407.578 150.337 407.578 242.057H365.16C365.16 173.764 309.798 118.401 241.505 118.401C173.212 118.401 117.85 173.764 117.85 242.057H75.432C75.432 150.337 149.785 75.9838 241.505 75.9838Z"
+                        fill="#336BB7" />
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M241.505 0.700195C374.802 0.700207 482.861 108.759 482.861 242.057H440.444C440.444 132.186 351.376 43.1178 241.505 43.1178C131.634 43.1178 42.5661 132.186 42.5661 242.057H0.148438C0.148449 108.759 108.207 0.700184 241.505 0.700195Z"
+                        fill="#336BB7" />
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M241.505 147.845C293.536 147.845 335.716 190.025 335.716 242.057H293.298C293.298 213.452 270.11 190.263 241.505 190.263C212.9 190.263 189.711 213.452 189.711 242.057H147.294C147.294 190.025 189.474 147.845 241.505 147.845Z"
+                        fill="#336BB7" />
+                </svg>
+
+            </form>
+
+            <div class="content">
+
+
+                <!-- first -->
+                <div class="first">
+                    <img src="../public/images/logo.png" alt="logo">
+                    <p>Anand Children Hospital is one of the largest Pediatric Super Specialty Hospital of South Gujarat
+                        located at the center of the Surat City</p>
+                    <div class="icon-text">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                            <path
+                                d="M9.9999 3.875C6.01657 3.875 2.7749 7.11667 2.7749 11.1C2.7749 15.0833 6.01657 18.3333 9.9999 18.3333C13.9832 18.3333 17.2249 15.0917 17.2249 11.1083C17.2249 7.125 13.9832 3.875 9.9999 3.875ZM10.6249 10.8333C10.6249 11.175 10.3416 11.4583 9.9999 11.4583C9.65824 11.4583 9.3749 11.175 9.3749 10.8333V6.66667C9.3749 6.325 9.65824 6.04167 9.9999 6.04167C10.3416 6.04167 10.6249 6.325 10.6249 6.66667V10.8333Z"
+                                fill="#0098DA" />
+                            <path
+                                d="M12.4084 2.87484H7.5917C7.25837 2.87484 6.9917 2.60817 6.9917 2.27484C6.9917 1.9415 7.25837 1.6665 7.5917 1.6665H12.4084C12.7417 1.6665 13.0084 1.93317 13.0084 2.2665C13.0084 2.59984 12.7417 2.87484 12.4084 2.87484Z"
+                                fill="#0098DA" />
+                        </svg>
+                        <span>09 AM - 10 PM , Monday - Saturday</span>
+                    </div>
+
+                    <div class="icon-text">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                            <path
+                                d="M9.9999 3.875C6.01657 3.875 2.7749 7.11667 2.7749 11.1C2.7749 15.0833 6.01657 18.3333 9.9999 18.3333C13.9832 18.3333 17.2249 15.0917 17.2249 11.1083C17.2249 7.125 13.9832 3.875 9.9999 3.875ZM10.6249 10.8333C10.6249 11.175 10.3416 11.4583 9.9999 11.4583C9.65824 11.4583 9.3749 11.175 9.3749 10.8333V6.66667C9.3749 6.325 9.65824 6.04167 9.9999 6.04167C10.3416 6.04167 10.6249 6.325 10.6249 6.66667V10.8333Z"
+                                fill="#0098DA" />
+                            <path
+                                d="M12.4084 2.87484H7.5917C7.25837 2.87484 6.9917 2.60817 6.9917 2.27484C6.9917 1.9415 7.25837 1.6665 7.5917 1.6665H12.4084C12.7417 1.6665 13.0084 1.93317 13.0084 2.2665C13.0084 2.59984 12.7417 2.87484 12.4084 2.87484Z"
+                                fill="#0098DA" />
+                        </svg>
+                        <span>10 AM - 07 PM , Sunday</span>
+                    </div>
+                </div>
+
+                <!-- second -->
+                <div class="second">
+                    <h4>Quick Links</h4>
+                    <NuxtLink to="#">Home</NuxtLink>
+                    <NuxtLink to="#">About Us</NuxtLink>
+                    <NuxtLink to="#">Services</NuxtLink>
+                    <NuxtLink to="#">Appointment</NuxtLink>
+                    <NuxtLink to="#">Blogs</NuxtLink>
+                    <NuxtLink to="#">Contact</NuxtLink>
+                </div>
+
+                <!-- third -->
+                <div class="third">
+                    <h4>Useful Links</h4>
+                    <NuxtLink to="#">Privacy Policy</NuxtLink>
+                    <NuxtLink to="#">Terms and Conditions</NuxtLink>
+                    <NuxtLink to="#">Disclaimer</NuxtLink>
+                    <NuxtLink to="#">Support</NuxtLink>
+                    <NuxtLink to="#">FAQ</NuxtLink>
+                </div>
+
+                <!-- fourth -->
+                <div class="fourth">
+                    <h4>Get in Touch</h4>
+                    <p>1/568 B Por Mohallo, Athugar Street, Near Kailash Restaurant, Nanpura, Surat - 395001</p>
+                    <div class="icon-text">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
+                            <path
+                                d="M14.875 3.0625H6.125C3.5 3.0625 1.75 4.375 1.75 7.4375V13.5625C1.75 16.625 3.5 17.9375 6.125 17.9375H14.875C17.5 17.9375 19.25 16.625 19.25 13.5625V7.4375C19.25 4.375 17.5 3.0625 14.875 3.0625ZM15.2862 8.39125L12.5475 10.5788C11.97 11.0425 11.235 11.27 10.5 11.27C9.765 11.27 9.02125 11.0425 8.4525 10.5788L5.71375 8.39125C5.43375 8.16375 5.39 7.74375 5.60875 7.46375C5.83625 7.18375 6.2475 7.13125 6.5275 7.35875L9.26625 9.54625C9.93125 10.08 11.06 10.08 11.725 9.54625L14.4638 7.35875C14.7438 7.13125 15.1637 7.175 15.3825 7.46375C15.61 7.74375 15.5662 8.16375 15.2862 8.39125Z"
+                                fill="#0098DA" />
+                        </svg>
+                        <span>NICU - 99251 00141, PICU - 9879612932</span>
+                    </div>
+
+                    <div class="icon-text">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
+                            <path
+                                d="M9.66875 13.0813L8.05 14.7C7.70875 15.0413 7.16625 15.0413 6.81625 14.7088C6.72 14.6125 6.62375 14.525 6.5275 14.4288C5.62625 13.5188 4.8125 12.565 4.08625 11.5675C3.36875 10.57 2.79125 9.5725 2.37125 8.58375C1.96 7.58625 1.75 6.6325 1.75 5.7225C1.75 5.1275 1.855 4.55875 2.065 4.03375C2.275 3.5 2.6075 3.01 3.07125 2.5725C3.63125 2.02125 4.24375 1.75 4.89125 1.75C5.13625 1.75 5.38125 1.8025 5.6 1.9075C5.8275 2.0125 6.02875 2.17 6.18625 2.3975L8.21625 5.25875C8.37375 5.4775 8.4875 5.67875 8.56625 5.87125C8.645 6.055 8.68875 6.23875 8.68875 6.405C8.68875 6.615 8.6275 6.825 8.505 7.02625C8.39125 7.2275 8.225 7.4375 8.015 7.6475L7.35 8.33875C7.25375 8.435 7.21 8.54875 7.21 8.68875C7.21 8.75875 7.21875 8.82 7.23625 8.89C7.2625 8.96 7.28875 9.0125 7.30625 9.065C7.46375 9.35375 7.735 9.73 8.12 10.185C8.51375 10.64 8.93375 11.1038 9.38875 11.5675C9.47625 11.655 9.5725 11.7425 9.66 11.83C10.01 12.1713 10.0188 12.7313 9.66875 13.0813Z"
+                                fill="#0098DA" />
+                            <path
+                                d="M19.2237 16.0389C19.2237 16.2839 19.1799 16.5377 19.0924 16.7827C19.0662 16.8527 19.0399 16.9227 19.0049 16.9927C18.8562 17.3077 18.6637 17.6052 18.4099 17.8852C17.9812 18.3577 17.5087 18.6989 16.9749 18.9177C16.9662 18.9177 16.9574 18.9264 16.9487 18.9264C16.4324 19.1364 15.8724 19.2502 15.2687 19.2502C14.3762 19.2502 13.4224 19.0402 12.4162 18.6114C11.4099 18.1827 10.4037 17.6052 9.40615 16.8789C9.0649 16.6252 8.72365 16.3714 8.3999 16.1002L11.2612 13.2389C11.5062 13.4227 11.7249 13.5627 11.9087 13.6589C11.9524 13.6764 12.0049 13.7027 12.0662 13.7289C12.1362 13.7552 12.2062 13.7639 12.2849 13.7639C12.4337 13.7639 12.5474 13.7114 12.6437 13.6152L13.3087 12.9589C13.5274 12.7402 13.7374 12.5739 13.9387 12.4689C14.1399 12.3464 14.3412 12.2852 14.5599 12.2852C14.7262 12.2852 14.9012 12.3202 15.0937 12.3989C15.2862 12.4777 15.4874 12.5914 15.7062 12.7402L18.6024 14.7964C18.8299 14.9539 18.9874 15.1377 19.0837 15.3564C19.1712 15.5752 19.2237 15.7939 19.2237 16.0389Z"
+                                fill="#0098DA" />
+                        </svg>
+                        <span>Anandchildrenhospital@yahoo.com</span>
+                    </div>
+
+                </div>
+            </div>
+
+            <hr>
+            <div class="copyright">
+                <p>Visitor Count : 87872 | Anand Children Hospital. All right reserved. | Developed By : ABC</p>
+                <div class="socials">
+                    <a href="#">
+                        <img src="../public/images/icons/insta-icon.png" alt="insta">
+                    </a>
+                    <a href="#">
+                        <img src="../public/images/icons/facebook-icon.png" alt="facebook">
+                    </a>
+                    <a href="#">
+                        <img src="../public/images/icons/whatsapp-icon.png" alt="whatsapp">
+                    </a>
+                </div>
+            </div>
+        </div>
     </footer>
 </template>
 <style scoped>
+/* form */
 
-</style>
+form.desktop {
+    height: 265px;
+    background: linear-gradient(335deg, #0098DA 0%, #A8CF45 100%);
+    border-radius: 30px;
+    position: absolute;
+    transform: translateY(-50%);
+    top: 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+form .inner {
+    width: 100%;
+}
+
+form .circle{
+    position: absolute;
+    z-index: -1;
+    left: 96px;
+    bottom: -4px;
+    opacity: 0.15;
+}
+
+form .input {
+    background-color: white;
+    max-width: 764px;
+    height: 76px;
+    border-radius: 60px;
+    margin: auto;
+    display: grid;
+    grid-template-columns: 1fr max-content;
+    padding: 8px;
+}
+
+form input {
+    background-color: transparent;
+    height: 100%;
+    padding: 0 40px;
+    font-size: 16px;
+    outline: none;
+    border: none;
+    background: linear-gradient(319deg, #0098DA 0%, #A8CF45 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+
+form h3 {
+    text-align: center;
+    color: white;
+    margin-top: 0;
+    margin-bottom: 39px;
+}
+
+form button {
+    height: 100%;
+    width: 157px;
+    text-align: center;
+    border: none;
+    background-color: var(--color-primary);
+    border-radius: 40px;
+    color: white;
+    font-size: 16px;
+    box-shadow: 0px 17px 25px 0px rgba(0, 0, 0, 0.25);
+}
+
+
+
+
+
+/* footer */
+footer {
+    min-height: 670px;
+    margin-top: 240px;
+    background-color: black;
+}
+
+footer .page {
+    position: relative;
+}
+
+footer .content {
+    padding-top: 200px;
+    display: grid;
+    grid-template-columns: 2fr 1fr 1.1fr 2fr;
+    gap: 125px;
+}
+
+footer .content p {
+    color: white;
+}
+
+footer .content a {
+    display: block;
+    color: white;
+}
+
+
+footer .first img {
+    margin-bottom: 40px;
+    display: block;
+}
+
+footer .content p {
+    margin-bottom: 38px;
+}
+
+footer .icon-text {
+    display: grid;
+    grid-template-columns: max-content 1fr;
+    align-items: center;
+    color: white;
+    gap: 7px;
+    margin-bottom: 9px;
+}
+
+footer .icon-text span {
+    font-size: 14px;
+    display: block;
+}
+
+
+footer h4 {
+    margin-bottom: 32px;
+    background: linear-gradient(319deg, #0098DA 0%, #A8CF45 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+
+footer .content a {
+    margin-bottom: 10px;
+}
+
+
+footer .first p {
+    margin-bottom: 38px;
+}
+
+
+footer hr {
+    border: none;
+    border-bottom: 1px solid #404040;
+    margin-top: 74px;
+}
+
+
+
+/* copyright */
+
+footer .copyright {
+    height: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+footer .copyright p {
+    font-size: 14px;
+    color: #BFBFBF;
+}
+
+.copyright .socials {
+    display: flex;
+    align-items: center;
+    gap: 14px;
+}
+
+.copyright .socials a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 40px;
+    width: 40px;
+    border: 1px solid #404040;
+    border-radius: 50%;
+}</style>
