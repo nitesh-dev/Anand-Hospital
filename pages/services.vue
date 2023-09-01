f259d1b3f14324c76af3bdd3d50310572dd37c4e
nitesh-dev <niteshdev547@gmail.com>
2023-09-01 10:54:39 +0530

    server section completed

--- pages/services.vue
+++ pages/services.vue
@@ -7,358 +7,187 @@ import image from '../public/images/background/landing-service.png'
 </script>
 <template>
     <OtherLanding active="/services" :image="image" bread="Services" title="Services Our Hospital" />
+    <section class="facilities">
+        <div class="page">
+            <span>Our Facilities</span>
+            <h3>Latest Technologies for Your Health</h3>
 
-    <Question/>
+            <div class="holder">
+                <div class="left">
+                    <img src="../public/images/background/doctor3.png">
+                    <p>As a tertiary referral ICU to provide state of the art care with the help of very good professionals
+                        and infrastructure.</p>
+                </div>
+                <div class="right">
+                    <div class="top">
+                        <div class="content">
+                            <h4>laboratory Analytics</h4>
+                            <p>Anand Children Hospital is one of the largest Pediatric Super Specialty Hospital of South
+                                Gujarat located at the center of the Surat City.</p>
+                            <p>Our mission is "Towards Playful Health & Intact Survival with Humane Touch" for all.</p>
+                            <WidgetButton><span>More About Us..</span></WidgetButton>
+                        </div>
+                        <img src="../public/images/background/background4.png">
+                    </div>
+                    <hr>
+                    <div class="bottom">
+                        <div class="item">
+                            <div class="image-holder">
+                                <img src="../public/images/background/mini-background4.png">
+                                <div>
+                                    <h5>Load More</h5>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="12" viewBox="0 0 16 12"
+                                        fill="none">
+                                        <path
+                                            d="M15.5303 6.53033C15.8232 6.23744 15.8232 5.76256 15.5303 5.46967L10.7574 0.696699C10.4645 0.403806 9.98959 0.403806 9.6967 0.696699C9.40381 0.989593 9.40381 1.46447 9.6967 1.75736L13.9393 6L9.6967 10.2426C9.40381 10.5355 9.40381 11.0104 9.6967 11.3033C9.98959 11.5962 10.4645 11.5962 10.7574 11.3033L15.5303 6.53033ZM0 6.75H15V5.25H0V6.75Z"
+                                            fill="black" />
+                                    </svg>
+                                </div>
+                            </div>
+                            <p>Operation Theatre</p>
+                        </div>
+
+                        <div class="item">
+                            <img src="../public/images/background/mini-background1.png">
+                            <p>Laboratory Department</p>
+                        </div>
+
+                        <div class="item">
+                            <img src="../public/images/background/mini-background2.png">
+                            <p>NICU / PICU</p>
+                        </div>
+
+                        <div class="item">
+                            <img src="../public/images/background/mini-background3.png">
+                            <p>OPD & Ward</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <Question />
     <Footer></Footer>
 </template>
 
 
-<!-- Family-Centered Care
-Preventive Services
-Vaccinations and Immunizations -->
-
 
 <style scoped>
-.about2 {
-    min-height: 515px;
+.facilities {
     margin-top: 110px;
-    margin-bottom: 110px;
-
 }
 
-.about2 .page>span {
+.facilities .page>span {
+    text-align: center;
+    display: block;
     color: var(--color-primary);
-    margin-top: 0;
 }
 
-.about2 h3 {
-    margin-top: 17px;
-    max-width: 780px;
-    width: 100%;
-    margin-bottom: 0;
+.facilities h3 {
+    text-align: center;
+    margin: 0;
+    margin-top: 18px;
+    margin-bottom: 70px;
+
 }
 
-.about2 .container {
+.facilities .holder {
+    display: grid;
+    grid-template-columns: max-content 1fr;
+    gap: 32px;
+}
+
+/* left */
+.facilities .left {
+    position: relative;
+    widows: 372px;
+}
+
+.facilities .left img {
+    height: 100%;
+    border-radius: 30px;
+    object-fit: cover;
+}
+
+.facilities .left p {
+    position: absolute;
+    margin: 0;
+    left: 24px;
+    right: 24px;
+    bottom: 24px;
+    background-color: black;
+    border-radius: 30px;
+    color: white;
+    padding: 25px;
+    text-align: center;
+}
+
+
+/* right */
+.facilities .right h4 {
+    margin: 15px 0;
+}
+
+
+.facilities .right .top {
     display: grid;
     grid-template-columns: 1fr 1fr;
     gap: 60px;
 }
 
-
-.about2 .right {
-    min-height: 402px;
-    position: relative;
-}
-
-.about2 .right>img {
-    display: block;
-    height: 118px;
-    object-fit: cover;
-    border-radius: 30px;
-    box-shadow: 0px 4px 44px 0px rgba(0, 0, 0, 0.19);
-}
-
-.about2 .right .top {
-    margin-right: auto;
-    width: 272px;
-}
-
-.about2 .right .center {
-    margin-left: auto;
-    width: 345px;
-    margin-right: 20px;
-    margin-top: 24px;
-    margin-bottom: 24px;
-}
-
-.about2 .right .bottom {
-    margin-left: 80px;
-    width: 304px;
-
-}
-
-
-.about2 h5 {
-    font-size: 18px;
-    font-weight: 500;
-    font-family: 'Outfit';
-    margin: 0;
-    margin-bottom: 15px;
-    margin-top: 70px;
-    color: black;
-}
-
-
-.about2 button{
-    margin-top: 32px;
-}
-
-
-/* list */
-.about2 .list{
-    margin-left: 15px;
-    margin-top: 28px;
-}
-
-.about2 .list div{
-    display: flex;
-    align-items: center;
-    gap: 11px;
-}
-
-
-/* outline */
-.about2 .outline .star {
-    position: absolute;
-    z-index: -1;
-    right: 136px;
-    top: 40px;
-}
-
-.about2 .outline .circle {
-    position: absolute;
-    z-index: -1;
-    left: 80px;
-    bottom: 78px;
-
-}
-
-
-
-
-/* ----------------- features section ----------- */
-.features {
-    margin-top: 210px;
-    min-height: 615px;
-    background-color: black;
-    padding-top: 110px;
-    padding-bottom: 110px;
-}
-
-
-.features .info {
-    display: grid;
-    grid-template-columns: 1fr 1.3fr;
-    gap: 45px;
-}
-
-
-.features .right {
-    position: relative;
-}
-
-.features .right .card-holder {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    gap: 32px;
-    position: absolute;
-    top: -210px;
-}
-
-
-/* left */
-.features .info .left span {
-    color: var(--color-primary);
-}
-
-.features .info .left h3 {
-    color: white;
-    margin: 0;
-    margin-top: 18px;
-    margin-bottom: 24px;
-}
-
-.features .info .left p {
-    color: #E6E6E6;
-    max-width: calc(100% - 20px);
-}
-
-/* right card */
-.features .right .card {
-    background-color: white;
-    min-height: 340px;
-    border-radius: 30px;
-    padding: 40px;
-    box-shadow: 0px -3px 57px 0px rgba(0, 0, 0, 0.11);
-}
-
-.features .right .card .icon {
-    width: 92px;
-    height: 92px;
-    border-radius: 50%;
-    background-color: hsla(198, 100%, 43%, 0.2);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
-.features .right .card h4 {
-    margin-top: 23px;
-    margin-bottom: 13px;
-}
-
-.features .right .card a {
-    color: var(--color-primary);
-}
-
-
-
-
-/* ---- progress holder -------- */
-
-.features .progress-holder {
-    margin-top: 98px;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: 70px;
-}
-
-.features .progress-holder .heading {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-
-.features .progress-holder .heading h4 {
-    font-family: 'Outfit';
-    font-weight: normal;
-    color: white;
-    margin: 0;
-}
-
-.features .progress-holder .heading span {
-    color: white;
-}
-
-.features .progress-holder p {
-    color: #BFBFBF;
-    font-size: 18px;
-    margin-right: 20px;
-}
-
-.features .progress-holder .progress {
+.facilities .right .top img{
     width: 100%;
-    border-radius: 20px;
-    height: 7px;
-    background-color: hsla(0, 0%, 25%, 0.7);
-    margin-top: 20px;
-    margin-bottom: 24px;
+    border-bottom: 30px;
+    object-fit: cover;
 }
 
-.features .progress-holder .progress .fill {
-    width: 90%;
-    height: 100%;
-    background: linear-gradient(270deg, #0098DA 0%, rgba(142, 183, 38, 0.73) 100%);
-    border-radius: 20px;
+
+.facilities hr{
+    border: none;
+    border-top: 1px solid #E6E6E6;
+    margin: 37px 0;
 }
 
 
 
-
-
-/* --------------------- specialist section -------------- */
-
-.specialist {
-    margin-top: 110px;
-}
-
-.specialist .page>span {
-    text-align: center;
-    display: block;
-    color: var(--color-primary);
-}
-
-.specialist h3 {
-    text-align: center;
-    margin: 0;
-    margin-top: 18px;
-    margin-bottom: 32px;
-}
-
-.specialist .page>p {
-    text-align: center;
-}
-
-.specialist .card-holder {
+/* bottom */
+.facilities .bottom {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr;
     gap: 32px;
-    margin-top: 62px;
 }
 
-.specialist .card {
-    box-shadow: 0px 0px 42px 0px rgba(0, 0, 0, 0.10);
-    padding: 24px;
-    border-radius: 30px;
+.facilities .bottom .item p{
+    margin: 17px 0;
+    text-align: center;
+    color: var(--color-on-surface);
+    font-weight: 500;
 }
 
-
-
-/* heading */
-
-.specialist .card>img {
+.facilities .bottom img{
     width: 100%;
-    border-radius: 30px;
+    aspect-ratio: 1/1;
 }
 
-.specialist .heading {
-    height: 77px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: calc(100% - 20px);
-    margin: auto;
-    background: linear-gradient(319deg, #0098DA 0%, #A8CF45 100%);
-    border-radius: 30px;
-    transform: translateY(-50%);
+.facilities .bottom .item .image-holder{
+    position: relative;
 }
 
-.specialist .heading h5,
-.specialist .heading span {
-    color: white;
-    font-weight: normal;
-}
-
-.specialist .heading span {
-    opacity: 0.8;
-}
-
-.specialist .heading h5 {
-    margin: 0;
+.facilities .bottom .item .image-holder h5{
     font-size: 18px;
-    text-align: center;
 }
 
-.specialist .heading span {
-    display: block;
-    font-size: 14px;
-    text-align: center;
-}
-
-
-/* done */
-.specialist .done {
+.facilities .bottom .image-holder div{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
     display: flex;
     align-items: center;
+    gap: 12px;
     justify-content: center;
-    gap: 9px;
-    transform: translateY(-15px);
 }
-
-.specialist .done p {
-    margin: 0;
-}
-
-/* rating */
-.specialist .rating {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 9px;
-}
-
-
-
-
-/* ---------------------------  */</style>
+</style>
