e2c02df232399b32afc81711173c7a025325f3c1
nitesh-dev <niteshdev547@gmail.com>
2023-09-02 22:08:44 +0530

    contact page done

---
+++ pages/contact.vue
@@ -0,0 +1,444 @@
+<script setup lang='ts'>
+import image from '../public/images/background/landing-service.png'
+
+
+const data = ['Search Nearest Clinic Of Bootamins', 'Select Your Services Option', 'Fill Out The Appointment Form', 'Meet With Doctor At Bootamins Clinic']
+
+
+
+</script>
+<template>
+    <OtherLanding active="/contact" :image="image" bread="Contact Us" title="Contact Us" />
+
+    <section class="contact">
+        <div class="page">
+            <span>Contact Us</span>
+            <h3>Get In Touch</h3>
+            <p>A Big & Dedicated Team of Experienced Pediatricians to provide Quality Healthcare.</p>
+
+            <div class="holder">
+                <div class="circle first">
+                    <div class="inner">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path
+                                    d="M11.05 14.95L9.2 16.8C8.81 17.19 8.19 17.19 7.79 16.81C7.68 16.7 7.57 16.6 7.46 16.49C6.43 15.45 5.5 14.36 4.67 13.22C3.85 12.08 3.19 10.94 2.71 9.81C2.24 8.67 2 7.58 2 6.54C2 5.86 2.12 5.21 2.36 4.61C2.6 4 2.98 3.44 3.51 2.94C4.15 2.31 4.85 2 5.59 2C5.87 2 6.15 2.06 6.4 2.18C6.66 2.3 6.89 2.48 7.07 2.74L9.39 6.01C9.57 6.26 9.7 6.49 9.79 6.71C9.88 6.92 9.93 7.13 9.93 7.32C9.93 7.56 9.86 7.8 9.72 8.03C9.59 8.26 9.4 8.5 9.16 8.74L8.4 9.53C8.29 9.64 8.24 9.77 8.24 9.93C8.24 10.01 8.25 10.08 8.27 10.16C8.3 10.24 8.33 10.3 8.35 10.36C8.53 10.69 8.84 11.12 9.28 11.64C9.73 12.16 10.21 12.69 10.73 13.22C10.83 13.32 10.94 13.42 11.04 13.52C11.44 13.91 11.45 14.55 11.05 14.95Z"
+                                    fill="#113C4E" />
+                                <path
+                                    d="M21.9696 18.33C21.9696 18.61 21.9196 18.9 21.8196 19.18C21.7896 19.26 21.7596 19.34 21.7196 19.42C21.5496 19.78 21.3296 20.12 21.0396 20.44C20.5496 20.98 20.0096 21.37 19.3996 21.62C19.3896 21.62 19.3796 21.63 19.3696 21.63C18.7796 21.87 18.1396 22 17.4496 22C16.4296 22 15.3396 21.76 14.1896 21.27C13.0396 20.78 11.8896 20.12 10.7496 19.29C10.3596 19 9.96961 18.71 9.59961 18.4L12.8696 15.13C13.1496 15.34 13.3996 15.5 13.6096 15.61C13.6596 15.63 13.7196 15.66 13.7896 15.69C13.8696 15.72 13.9496 15.73 14.0396 15.73C14.2096 15.73 14.3396 15.67 14.4496 15.56L15.2096 14.81C15.4596 14.56 15.6996 14.37 15.9296 14.25C16.1596 14.11 16.3896 14.04 16.6396 14.04C16.8296 14.04 17.0296 14.08 17.2496 14.17C17.4696 14.26 17.6996 14.39 17.9496 14.56L21.2596 16.91C21.5196 17.09 21.6996 17.3 21.8096 17.55C21.9096 17.8 21.9696 18.05 21.9696 18.33Z"
+                                    fill="#113C4E" />
+                            </svg>
+                            <span>Call Us</span>
+                        </div>
+                        <span>+62-361-234-4567 |</span>
+                        <span>Hotline 112-110-5</span>
+
+                        <div class="image-holder">
+                            <img src="../public/images/background/circle-back1.png">
+                        </div>
+                        <div class="line">
+                            <span>01</span>
+                            <hr>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="circle second">
+                    <div class="inner">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path
+                                    d="M17 3.5H7C4 3.5 2 5 2 8.5V15.5C2 19 4 20.5 7 20.5H17C20 20.5 22 19 22 15.5V8.5C22 5 20 3.5 17 3.5ZM17.47 9.59L14.34 12.09C13.68 12.62 12.84 12.88 12 12.88C11.16 12.88 10.31 12.62 9.66 12.09L6.53 9.59C6.21 9.33 6.16 8.85 6.41 8.53C6.67 8.21 7.14 8.15 7.46 8.41L10.59 10.91C11.35 11.52 12.64 11.52 13.4 10.91L16.53 8.41C16.85 8.15 17.33 8.2 17.58 8.53C17.84 8.85 17.79 9.33 17.47 9.59Z"
+                                    fill="#113C4E" />
+                            </svg>
+                            <span>Email Us</span>
+                        </div>
+                        <span>anandchildrenhospital@gmail.com</span>
+                        <span>| support@info.com</span>
+
+                        <div class="image-holder">
+                            <img src="../public/images/background/circle-back2.png">
+                        </div>
+                        <div class="line">
+                            <hr>
+                            <span>02</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="circle third">
+                    <div class="inner">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path
+                                    d="M12 2C6.49 2 2 6.49 2 12C2 17.51 6.49 22 12 22C17.51 22 22 17.51 22 12C22 6.49 17.51 2 12 2ZM16.35 15.57C16.21 15.81 15.96 15.94 15.7 15.94C15.57 15.94 15.44 15.91 15.32 15.83L12.22 13.98C11.45 13.52 10.88 12.51 10.88 11.62V7.52C10.88 7.11 11.22 6.77 11.63 6.77C12.04 6.77 12.38 7.11 12.38 7.52V11.62C12.38 11.98 12.68 12.51 12.99 12.69L16.09 14.54C16.45 14.75 16.57 15.21 16.35 15.57Z"
+                                    fill="#113C4E" />
+                            </svg>
+                            <span>Working Hours</span>
+                        </div>
+                        <span>Monday - Sunday :</span>
+                        <span>9:30 AM to 7:00 PM</span>
+
+                        <div class="image-holder">
+                            <img src="../public/images/background/circle-back3.png">
+                        </div>
+                        <div class="line">
+                            <span>03</span>
+                            <hr>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="contact-area">
+        <div class="page">
+            <form>
+                <div class="col-2">
+                    <div class="input">
+                        <span>First Name</span>
+                        <div class="input-holder">
+                            <input type="text" placeholder="First Name">
+                        </div>
+                    </div>
+                    <div class="input">
+                        <span>Last Name</span>
+                        <div class="input-holder">
+                            <input type="text" placeholder="Last Name">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="input">
+                    <span>Your email</span>
+                    <div class="input-holder">
+                        <input type="email" placeholder="Your Email">
+                    </div>
+                </div>
+
+                <div class="input">
+                    <span>Telephone</span>
+                    <div class="input-holder">
+                        <input type="text" placeholder="Your Phone">
+                    </div>
+                </div>
+
+                <div class="input">
+                    <span>Message</span>
+                    <div class="input-holder-textarea">
+                        <textarea rows="6" placeholder="Your Comment"></textarea>
+                    </div>
+                </div>
+
+                <WidgetButton type="submit"><span>Send Message</span></WidgetButton>
+            </form>
+
+            <div class="right">
+                <span>Contact Now</span>
+                <h3>Leave A Message</h3>
+                <p>we offer a wide range of comprehensive healthcare services to address all aspects of your health. </p>
+                <div class="map">
+                    <img src="../public/images/background/map.png" alt="map">
+                    <div class="card">
+                        <h5>Ngurah Rai Immigration Office</h5>
+                        <p>Jl. Raya Taman Jimbaran No.1, Jimbaran, Kec. Kuta Sel., Kabupaten Badung, Bali 80361, Indonesia</p>
+                        <div class="rating">
+                            <span>4.7</span>
+                            <div class="stars">
+                                <img v-for="item in 5" src="../public/images/icons/star.png" alt="star">
+                            </div>
+                            <span>925 Reviews</span>
+                        </div>
+
+                        <a href="#">View Larger Map</a>
+
+                        <img class="direction" src="../public/images/icons/direction.png" alt="direction">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="page shadow">
+        <div></div>
+    </div>
+
+    <Footer></Footer>
+</template>
+
+
+
+<style scoped>
+
+
+/* ----------------- contact area -------------- */
+.contact-area{
+    background-color: black;
+    padding-top: 114px;
+    padding-bottom: 140px;
+}
+
+.contact-area .page{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+}
+
+.contact-area .right>span{
+    color: var(--color-primary);
+    margin-bottom: 16px;
+    display: block;
+}
+
+.contact-area .right h3{
+    color: white;
+    font-size: 32px;
+    margin: 0;
+}
+
+.contact-area .right{
+    display: flex;
+    flex-direction: column;
+}
+
+.contact-area .right>p{
+    margin-top: 24px;
+    color: #E6E6E6;
+    margin-bottom: 36px;
+}
+
+.contact-area .map{
+    flex-grow: 1;
+    position: relative;
+}
+.contact-area .map img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 30px;
+}
+
+.contact-area .map .card{
+    position: absolute;
+    left: 0;
+    top: 0;
+    margin: 24px;
+    background-color: white;
+    max-width: 312px;
+    padding: 16px;
+    border-radius: 30px;
+    box-shadow: 0px 1px 26px 0px rgba(0, 0, 0, 0.25);
+}
+
+.contact-area .map h5{
+    font-size: 18px;
+    color: black;
+    margin: 0;
+}
+
+.contact-area .map p{
+    font-size: 14px;
+    color: #7F7F7F;
+    margin-bottom: 8px;
+
+}
+
+.contact-area .rating{
+    display: flex;
+    gap: 6px;
+    align-items: center;
+}
+
+.contact-area .rating span:first-child{
+    font-size: 14px;
+}
+
+.contact-area .rating span:last-child{
+    font-size: 14px;
+    color: var(--color-primary);
+}
+
+.contact-area .rating .stars{
+    display: flex;
+    align-items: center;
+    gap: 2px;
+}
+
+.contact-area .rating .stars img{
+    width: 14px;
+    height: 14px;
+}
+
+.contact-area .map a{
+    font-size: 14px;
+    color: var(--color-primary);
+    margin-top: 10px;
+    margin-bottom: 8px;
+    display: block;
+}
+
+.contact-area .map .card .direction{
+    position: absolute;
+    right: 12px;
+    bottom: 24px;
+    width: 60px;
+    height: auto;
+    object-fit: contain;
+    border-radius: 0;
+}
+
+
+
+
+
+
+/* ---------------------- contact section ------------- */
+.contact {
+    margin-bottom: 216px;
+}
+.contact .page>span {
+    color: var(--color-primary);
+    display: block;
+    margin-bottom: 18px;
+    text-align: center;
+}
+
+.contact h3 {
+    margin: 0;
+    text-align: center;
+}
+
+
+.contact p {
+    margin: 32px 0;
+    text-align: center;
+}
+
+.contact .holder {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    margin-top: 160px;
+}
+
+.contact .circle {
+    position: relative;
+    width: 100%;
+    aspect-ratio: 1/1;
+    border: 1px solid black;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
+
+.contact .circle .image-holder{
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    clip-path: circle(50% at 50% 50%);
+    z-index: -10;
+}
+
+.contact .circle img{
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 118px;
+    z-index: -10;
+}
+
+.contact .circle .line {
+    border: none;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    top: -120px;
+    left: 50%;
+    transform: translateX(-50%);
+    gap: 0;
+}
+
+
+.contact .circle.second .line {
+    top: unset;
+    bottom: -120px;
+}
+
+.contact .circle .line span{
+    font-size: 32px;
+    font-weight: 600;
+    color: black;
+}
+
+.contact .circle .line hr{
+    height: 160px;
+    border-left: 1px solid black;
+}
+
+
+.contact .circle.first img, .contact .circle.third img{
+    bottom: 0;
+}
+
+.contact .circle.second img{
+    top: 0;
+}
+
+
+.contact .circle .inner div{
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 11px;
+}
+
+.contact .inner div span{
+    font-size: 24px;
+    font-weight: 600;
+    color: var(--color-on-surface);
+}
+
+.contact .inner span{
+    display: block;
+    text-align: center;
+    line-height: 161%;
+    font-size: 18px;
+    color: #7F7F7F;
+}
+
+.contact .circle.first {
+    transform: translateX(40px);
+}
+
+.contact .circle.third {
+    transform: translateX(-40px);
+}
+
+
+
+
+
+
+
+/* extra */
+
+.page.shadow {
+    position: relative;
+}
+
+.page.shadow div {
+    position: absolute;
+    width: 360px;
+    height: 360px;
+    right: 0;
+    background-color: var(--color-primary);
+    transform: translateY(-50%);
+    filter: blur(315px);
+    z-index: -1;
+}</style>
