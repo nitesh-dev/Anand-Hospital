ba424a3a1a4496586637ad002972a7c5c321bfff
nitesh-dev <niteshdev547@gmail.com>
2023-09-03 16:08:05 +0530

    bugs fixed in blogs

--- components/Blogs.vue
+++ components/Blogs.vue
@@ -1,5 +1,49 @@
 <script setup lang='ts'>
 import blog1 from '../public/images/blog/blog1.png'
+import blog2 from '../public/images/blog/blog2.png'
+import blog3 from '../public/images/blog/blog3.png'
+import blog4 from '../public/images/blog/blog4.png'
+
+const data = Array<{
+    image: string,
+    date: string,
+    title: string,
+    link: string
+}>()
+
+
+data.push({
+    image: blog1,
+    date: '· June 14, 2017',
+    title: 'Very important immunizations for children at the age of 12',
+    link: '#'
+})
+
+data.push({
+    image: blog2,
+    date: '· June 14, 2017',
+    title: 'Adapting to the development of children’s needs',
+    link: '#'
+})
+
+data.push({
+    image: blog3,
+    date: '· June 14, 2017',
+    title: '6 Tips For Maintaining Children’s Mental Health When Sick',
+    link: '#'
+})
+
+data.push({
+    image: blog4,
+    date: '· June 14, 2017',
+    title: 'Very important immunizations for children at the age of 12',
+    link: '#'
+})
+
+
+
+
+
 </script>
 <template>
     <section>
@@ -9,12 +53,12 @@ import blog1 from '../public/images/blog/blog1.png'
             <p>A Big & Dedicated Team of Experienced Pediatricians to provide Quality Healthcare.</p>
 
             <div class="card-holder hide-scroll">
-                <div class="card" v-for="item  in 10">
-                    <img :src="blog1">
+                <div class="card" v-for="item  in data">
+                    <img :src="item.image">
                     <div class="content">
-                        <span>· June 14, 2017</span>
-                        <p>6 Tips For Maintaining Children's Mental Health When Sick</p>
-                        <NuxtLink to="#">Read More..</NuxtLink>
+                        <span>{{ item.date }}</span>
+                        <p>{{ item.title }}</p>
+                        <NuxtLink :to="item.link">Read More..</NuxtLink>
                     </div>
                 </div>
             </div>
@@ -26,14 +70,12 @@ import blog1 from '../public/images/blog/blog1.png'
     </section>
 </template>
 <style scoped>
-
-
-section{
+section {
     margin-top: 110px;
     position: relative;
 }
 
-section .outline{
+section .outline {
     width: 211px;
     height: 211px;
     border-radius: 50%;
@@ -63,7 +105,7 @@ section .page>p {
 }
 
 
-.card-holder{
+.card-holder {
     width: 100%;
     overflow: auto;
     display: flex;
@@ -74,7 +116,7 @@ section .page>p {
     margin-top: 50px;
 }
 
-.card-holder .card{
+.card-holder .card {
     background-color: hsla(198, 100%, 43%, 0.07);
     min-width: 335px;
     padding: 16px;
@@ -85,36 +127,36 @@ section .page>p {
 }
 
 
-.card img{
+.card img {
     width: 100%;
     height: 164px;
     border-radius: 30px;
 }
 
-.card-holder .card:hover{
+.card-holder .card:hover {
     background-color: white;
     box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
 }
 
 
-.card span{
+.card span {
     margin-top: 30px;
     display: block;
     color: var(--color-secondary);
 }
 
-.card p{
+.card p {
     font-size: 18px;
     color: var(--color-on-surface);
     margin: 0.5em 0;
 }
 
-.card a{
+.card a {
     color: var(--color-primary);
     font-weight: 500;
 }
 
-button{
+button {
     margin: auto;
     margin-top: 20px;
 }
